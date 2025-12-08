e9685f048008bb7864085620ac44257c5e2d7667
Wayne Gakuo <waynejotham@gmail.com>
2025-12-08 16:24:46 +0300

    chore(route): update route URLs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/work-in-progress/work-in-progress').then(m => m.WorkInProgress)
+    loadComponent: () => import('./pages/home/home').then(m => m.Home)
   },
   {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    path: 'wip',
+    loadComponent: () => import('./pages/work-in-progress/work-in-progress').then(m => m.WorkInProgress)
   },
 ];
